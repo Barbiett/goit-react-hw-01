6400492bb0944c11ec5c31f71ed2eb014dd1ade0
Barbiet <bogdanvinokurov2000@gmail.com>
2024-04-25 11:54:56 +0300

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (<>
+        <h1>Page title</h1>
+        <button>click me</button>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Odio consequuntur laborum facere, esse asperiores
+            nam ipsum eveniet! Hic, nostrum id.</p>
+    </>);
+}
